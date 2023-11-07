2d5a5facce74d0b77b4a14dd4184118b5b6b5eb3
Loumber <92634780+Loumber@users.noreply.github.com>
2023-11-07 16:23:06 +0000

    Поправил reg_controller и act_controller

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -15,6 +15,6 @@ class RegistrationController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit()
   end
 end
