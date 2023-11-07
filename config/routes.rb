7fe638892fb0288bbd0a1bf169b7d8e08f10b838
VladimirGladky <92906509+VladimirGladky@users.noreply.github.com>
2023-11-07 16:12:13 +0000

    изменил роутинги

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'pages#index'
+  get '/welcome', to: 'pages#welcome'
+
+  get '/registration', to: 'registration#reg'
+  post '/registration', to: 'registration#create'
+
+  get '/auth', to: 'auth#reg'
+  post '/auth', to: 'auth#create'
+
+  get '/team',to: 'pages#team_overview'
   
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
