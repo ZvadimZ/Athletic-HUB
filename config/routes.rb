8dd35029e11cec94b3f4a6ac49de4ad838bc4aad
Katrina0373 <95878941+Katrina0373@users.noreply.github.com>
2023-12-05 22:20:32 +0300

    suffering

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   get '/team/:event_id', to: 'teams#index'
   post '/team/add_user', to: 'teams#create'
 
+  
+  get '/registration', to: 'registration#new'
+  post '/registration', to: 'registration#create'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
