473370d5ca6b5e98f7a12c13a045dd412e83853d
ZvadimZ <vadim.uvarov2003@outlook.com>
2023-12-05 21:49:28 +0300

    work with rest api

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  
+
   resources :events do
-    resources :teams, only: [:new, :create, :index]
+    resources :teams, only: [:new]
   end
 
   post '/events/:event_id/teams/:id/apply', to: 'teams#apply', as: :apply_team
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   get '/events/new', to: 'events#new'
   post '/events', to: 'events#create'
 
@@ -17,11 +17,13 @@ Rails.application.routes.draw do
   get '/login', to: 'login#new'
   post '/login', to: 'login#create'
   get '/team', to: 'pages#team_overview'
-  
+  get '/team/:event_id', to: 'teams#index'
+  post '/team/add_user', to: 'teams#create'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  
+
 
   # Defines the root path route ("/")
   # root "posts#index"
